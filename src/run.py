091db7617e2db1a73937548dcb65d8db120886fd
mrezaei <moeinrezaei330@gmail.com>
2023-01-08 19:46:46 +0100

    Initializing.

---
+++ src/run.py
@@ -0,0 +1,15 @@
+# *************************************************************************** */
+#                                                                             */
+#                                                                             */
+#    assignment.py                                                            */
+#                                                                             */
+#    By: moeinrz <moeinrezaei330@gmail.com>                                   */
+#                                                                             */
+#    Created: 2022/12/18 22:04:59 by moeinrz                                  */
+#    Updated: 2022/12/18 23:45:49 by moeinrz                                  */
+#                                                                             */
+# *************************************************************************** */
+
+import streamlit as st
+
+st.title(':zap: Moein Dashbpard')
