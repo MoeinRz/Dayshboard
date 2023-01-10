53b02fdd0081a8f011179ceda35dcf2d98ecaeda
MoeinRz <moeinrezaei330@gmail.com>
2023-01-10 21:16:15 +0100

    new function added.

--- src/run.py
+++ src/run.py
@@ -48,3 +48,16 @@ picture = st.camera_input("Take a picture")
 
 if picture:
     st.image(picture)
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
