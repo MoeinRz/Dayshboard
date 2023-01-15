6a03fbb8fd82dcd0f4d3c83b09781a7417fb94f4
mrezaei <moeinrezaei330@gmail.com>
2023-01-15 21:55:56 +0100

    run.py updated

--- src/run.py
+++ src/run.py
@@ -10,60 +10,68 @@
 #                                                                             */
 # *************************************************************************** */
 
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 import streamlit as st
-
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif"
-
-st.title(':zap: Moein Dashbpard')
-
-import streamlit as st
-agree = st.checkbox('I agree')
-
-if agree:
-    st.write('Great!')
-
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-
 from PIL import Image
-image = Image.open('pexel.jpg')
+import os
 
-st.image(image, caption='Sunrise by the mountains')
+import sys
 
-picture = st.camera_input("Take a picture")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+from db.models import Message
 
-if picture:
-    st.image(picture)
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
+# Login
+login_option = st.sidebar.radio('Login/Singup', ('Login', 'Singup'))
+if login_option == 'Login':
+    with st.sidebar.form("login"):
+        st.write("Login Here.")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
 
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Login")
+        if submitted:
+            pass
+else:
+    with st.sidebar.form("signup"):
+        st.write("Singup Here.")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        email = st.text_input("Email")
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Singup")
+        if submitted:
+            pass
+
+# Banner
+banner = Image.open('./data/banner.webp', )
+st.image(banner)
+st.title(':zap: Pytopia Dashboard')
+
+# Questions
+with st.expander('Q / A'):
+    query = st.text_input('Search:')
+
+    # select top 10 from messages
+    for msg in Message.objects.all().order_by('-date'):
+        if not msg.text or msg.text[-1] not in '؟?':
+            continue
+
+        if query and query not in msg.text:
+            continue
+
+        col1, col2 = st.columns([1, 4])
+        col1.write(f'**{msg.user.username}**')
+        col2.write(msg.text.replace(query, f'**{query}**'))
+
+    col1, col2 = st.columns(2)
+    col1.button('< Previous')
+    col2.button('Next >')
